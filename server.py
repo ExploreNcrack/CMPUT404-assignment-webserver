0b47fe261d165d9ba9b8789375bcff39dc193bb9
ExploreNcrack <zhaozhen@ualberta.ca>
2019-01-25 14:27:19 -0700

    add my name

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 import socketserver
 import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Zhaozhen Liang
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
